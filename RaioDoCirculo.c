f9caf7d88320e2bc61126ab27f0cc3b2d14fba09
o.lorenzatoo <henriquelorensan@gmail.com>
2026-08-19 22:20:19 -0300

    Add files via upload

--- RaioDoCirculo.c
+++ RaioDoCirculo.c
@@ -3,17 +3,21 @@
 #define pi 3.14159
 
 int main(int argc, char *argv[]) {
-	
-	float raio, area, raioQuadrado;
 
-	printf("Insira o valor de R: \n");
-	scanf("%f", &raio);
+	float raio, area, raioQuadrado;
 	
-	raioQuadrado = raio*raio;
-	area= pi * raioQuadrado;
+	printf ("insira o valor de R: \n");
+	scanf ("%f" , &raio);
 	
-	printf("A Area do circulo de raio %f = %f", raio, area);
+	raioQuadrado = raio * raio;
+	area = pi * raioQuadrado;
 	
+	printf ("A Area do circulo de raio %f = %f" , raio, area);
+	 
+	 
+	int B,b,h,areaT;
+	 
+	printf ("Area do Trapezio de lado B : %f; b:%f h:%f = %f", scanf("%f", &B), scanf("%f", &b),scanf("%f",&h))
+ 
 	return 0;
 }
-
